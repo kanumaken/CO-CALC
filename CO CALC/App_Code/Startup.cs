e85e0aa2aa513e4fdc76bb63bdb718d3b6499685
ken <kenkanuma0@outlook.com>
2024-03-01 15:48:54 +0200

    Add project files.

---
+++ CO CALC/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CO_CALC.Startup))]
+namespace CO_CALC
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
